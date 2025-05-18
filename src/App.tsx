b424651e4713da304c1a3925883cc888177706c0
ari-abendstern <ari.abendstern@gmx.com>
2025-05-18 16:57:46 +0100

    create template

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import { AppShell, Burger, Group, MantineProvider, Skeleton } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { theme } from "./theme";
 
+//const navLinks = [""]
+
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  const [opened, { toggle }] = useDisclosure();
+  return (
+    <MantineProvider theme={theme}>
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            Logo?
+          </Group>
+        </AppShell.Header>
+        <AppShell.Navbar p="md">
+          Navbar
+          {Array(15)
+            .fill(0)
+            .map((_, index) => (
+              <Skeleton key={index} h={28} mt="sm" animate={false} />
+            ))}
+        </AppShell.Navbar>
+        <AppShell.Main>Main</AppShell.Main>
+      </AppShell>
+    </MantineProvider>
+  );
 }
